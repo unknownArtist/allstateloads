c01e7743f21ce830a5f2d9e466aac13ae021306a
sehrish habib <sehrish@gmail.com>
2012-07-17 05:20:45 -0700

    default changed

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -13,7 +13,20 @@ class IndexController extends Zend_Controller_Action
         //data = new Default_Model_DbTable_News();
 		//$offset = "2";
         //$this->view->data = $data->fetchAll($offset);
-		
+        $loads = new Default_Model_DbTable_Loads();
+
+         $l_data = $loads->fetchAll($where = null,'id DESC','7,0');
+            
+        $this->view->l_data = $l_data;
+
+
+
+        $trucks = new Dashboard_Model_NewTrucks();
+
+         $t_data = $trucks->fetchAll($where = null,'created DESC','7,0');
+         $this->view->t_data = $t_data;
+
+
     }
 
     public function aboutAction()
